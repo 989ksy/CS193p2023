dc088ee6e98b86c8403efb459cc65f019f594013
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-12-06 21:49:43 +0900

    add: classCode folder

---
+++ ClassCode/CS193pClassCode/CS193pClassCode/CS193pClassCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pClassCodeApp.swift
+//  CS193pClassCode
+//
+//  Created by Seungyeon Kim on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CS193pClassCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
